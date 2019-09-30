de3c053c0e3db0445d9504f38d9bd0f3984f2d3e
Neil <galaxyaurona@gmail.com>
2019-10-01 00:24:14 +1000

    Add not empty string validation

--- CarDealer/Model/Car.cs
+++ CarDealer/Model/Car.cs
@@ -8,19 +8,17 @@ namespace CarDealer.Model
 {
     public class Car
     {
-        [Required]
         public int Id { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Car make can't be empty or contains whitespace only")]
         public string Make { get; set; }
-        [Required]
-        [L]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Car model can't be empty or contains whitespace only")]
         public string Model { get; set; }
         [Required]
         [Range(1900, 2200 , ErrorMessage = "Year must be between 1900 and 2200")]
         public int Year { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Car Body can't be empty or contains whitespace only")]
         public string Body { get; set; }
-        [Required]
+
         public string Color { get; set; }
         [Required]
         [Range(0, Double.MaxValue, ErrorMessage = "Price must be >= 0")]
